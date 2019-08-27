5dfd06a6686a6a7449694d73d529905063f21dda
RogueM99 <sadsog99@gmail.com>
2019-08-26 20:07:23 -0400

    updated character movement and added raycaster

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 5f;
+    Rigidbody myRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        myRigidbody.AddForce(movement * speed);
     }
 }
