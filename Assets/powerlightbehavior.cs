57a1f1c18aa1c2241ca9eda50eea55f6f1fe6a46
RogueM99 <sadsog99@gmail.com>
2019-08-26 20:09:12 -0400

    updated movement and added racaster

---
+++ Assets/powerlightbehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerlightbehavior : MonoBehaviour
+{
+    void Update()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit: "+ layerMask);
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            // Debug.Log("Did not Hit");
+        }
+    }
+}
